761977e4f88cce95d3a0232fd6ef53df27de859e
brightliming <limingbright@yahoo.com>
2017-01-18 17:21:09 +0800

    my first Example

--- MybatisBase/src/main/java/com/learn/chapter3/po/User.java
+++ MybatisBase/src/main/java/com/learn/chapter3/po/User.java
@@ -1,9 +1,73 @@
 package com.learn.chapter3.po;
 
+import com.learn.chapter3.enums.Sex;
+
 /**
  * Created by bright on 17-1-18.
  */
 public class User {
     private long id;
     private String userName;
+    private String cnname;
+    private Sex sex;
+    private String email;
+    private String mobile;
+    private String note;
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCnname() {
+        return cnname;
+    }
+
+    public void setCnname(String cnname) {
+        this.cnname = cnname;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+
 }
